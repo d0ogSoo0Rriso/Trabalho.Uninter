7185759f8b7b4b6a4732ee91db6f27f401d9d3ce
d0ogSoo0Rriso <gutogutao2010@gmail.com>
2025-05-18 06:00:39 -0300

    Delete site directory

--- site/index.js
+++
@@ -1,6 +0,0 @@
-let botao = document.querySelector("#botao");
-let mensagem = document.querySelector("#mensagem");
-botao.addEventListener("click",msg);
-function msg(){
-    mensagem.innerHTML="Programar é como montar um móvel sem instruções... e com peças extras."
-}
