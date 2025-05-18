28106c5aa4306ba94e4421b1ae246b07ea03816a
d0ogSoo0Rriso <gutogutao2010@gmail.com>
2025-05-18 06:02:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector("#botao");
+let mensagem = document.querySelector("#mensagem");
+botao.addEventListener("click",msg);
+function msg(){
+    mensagem.innerHTML="Programar é como montar um móvel sem instruções... e com peças extras."
+}
